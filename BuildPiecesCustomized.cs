3307236dad3d783c814415de738782ddf772335b
Suden <20457922+shudnal@users.noreply.github.com>
2024-05-21 03:22:46 +0300

    Add project files.

---
+++ BuildPiecesCustomized.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using ServerSync;
+using UnityEngine;
+using System.Linq;
+
+namespace BuildPiecesCustomized
+{
+    [BepInPlugin(pluginID, pluginName, pluginVersion)]
+    [BepInIncompatibility("aedenthorn.BuildPieceTweaks")]
+    [BepInIncompatibility("TheSxW_EditMaterialProperties")]
+    internal class BuildPiecesCustomized : BaseUnityPlugin
+    {
+        const string pluginID = "shudnal.BuildPiecesCustomized";
+        const string pluginName = "Build Pieces Customized";
+        const string pluginVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(pluginID);
+
+        internal static readonly ConfigSync configSync = new ConfigSync(pluginID) { DisplayName = pluginName, CurrentVersion = pluginVersion, MinimumRequiredVersion = pluginVersion };
+
+        internal static ConfigEntry<bool> modEnabled;
+        internal static ConfigEntry<bool> configLocked;
+        internal static ConfigEntry<bool> loggingEnabled;
+
+        internal static ConfigEntry<string> toolsToPatchPieces;
+
+        internal static ConfigEntry<bool> globalClipEverything;
+        internal static ConfigEntry<bool> globalAllowedInDungeons;
+        internal static ConfigEntry<bool> globalRepairPiece;
+        internal static ConfigEntry<bool> globalCanBeRemoved;
+
+        internal static ConfigEntry<bool> globalAshDamageImmune;
+        internal static ConfigEntry<bool> globalNoRoofWear;
+        internal static ConfigEntry<bool> globalNoSupportWear;
+
+        internal static BuildPiecesCustomized instance;
+
+        private static readonly CustomSyncedValue<Dictionary<string, string>> configsJSON = new CustomSyncedValue<Dictionary<string, string>>(configSync, "JSON configs", new Dictionary<string, string>());
+
+        internal static readonly Dictionary<string, CustomPieceData> pieceData = new Dictionary<string, CustomPieceData>();
+        internal static readonly Dictionary<string, CraftingStation> craftingStations = new Dictionary<string, CraftingStation>();
+
+        private static DirectoryInfo pluginDirectory;
+        private static DirectoryInfo configDirectory;
+
+        private static FileSystemWatcher fileSystemWatcherPlugin;
+        private static FileSystemWatcher fileSystemWatcherConfig;
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+
+            instance = this;
+
+            pluginDirectory = new DirectoryInfo(Assembly.GetExecutingAssembly().Location).Parent;
+            configDirectory = new DirectoryInfo(Path.Combine(Paths.ConfigPath, pluginID));
+
+            ConfigInit();
+
+            _ = configSync.AddLockingConfigEntry(configLocked);
+
+            configsJSON.ValueChanged += new Action(LoadConfigs);
+
+            Game.isModded = true;
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+            instance = null;
+            harmony?.UnpatchSelf();
+        }
+
+        public static void LogInfo(object data)
+        {
+            if (loggingEnabled.Value)
+                instance.Logger.LogInfo(data);
+        }
+        public static void LogWarning(object data)
+        {
+            if (loggingEnabled.Value)
+                instance.Logger.LogWarning(data);
+        }
+
+        public void ConfigInit()
+        {
+            config("General", "NexusID", 2782, "Nexus mod ID for updates", false);
+
+            modEnabled = config("General", "Enabled", defaultValue: true, "Enable the mod.");
+            configLocked = config("General", "Lock Configuration", defaultValue: true, "Configuration is locked and can be changed by server admins only.");
+            loggingEnabled = config("General", "Logging enabled", defaultValue: false, "Enable logging. [Not Synced with Server]", false);
+
+            toolsToPatchPieces = config("General", "Tools list", defaultValue: "Hammer,Hoe", "Comma separated list of tool prefab name");
+
+            globalClipEverything = config("Global", "Clip everything", defaultValue: false, "All pieces will clip through each other");
+            globalAllowedInDungeons = Config.Bind("Global", "Allow in dungeons", defaultValue: false, "All pieces will be allowed to build in dungeons.");
+            globalRepairPiece = Config.Bind("Global", "Can be repaired", defaultValue: false, "All pieces will be repairable.");
+            globalCanBeRemoved = Config.Bind("Global", "Can be removed", defaultValue: false, "All pieces will be removeable.");
+            
+            globalAshDamageImmune = Config.Bind("Immunity", "Ash and lava", defaultValue: false, "All pieces will be immune to ash and lava damage.");
+            globalNoRoofWear = Config.Bind("Immunity", "Water damage", defaultValue: false, "All pieces will be immune to water damage.");
+            globalNoSupportWear = Config.Bind("Immunity", "Structural integrity", defaultValue: false, "All pieces will be immune to structural damage.");
+
+            InitCommands();
+        }
+
+        ConfigEntry<T> config<T>(string group, string name, T defaultValue, ConfigDescription description, bool synchronizedSetting = true)
+        {
+            ConfigEntry<T> configEntry = Config.Bind(group, name, defaultValue, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = configSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        ConfigEntry<T> config<T>(string group, string name, T defaultValue, string description, bool synchronizedSetting = true) => config(group, name, defaultValue, new ConfigDescription(description), synchronizedSetting);
+
+        public static void InitCommands()
+        {
+            new Terminal.ConsoleCommand("bpcsave", "[Prefab name] - save piece data into JSON file next to mod", delegate (Terminal.ConsoleEventArgs args)
+            {
+                if (args.Length >= 2)
+                {
+                    string prefabName = args.FullLine.Substring(args[0].Length + 1);
+                    CustomPieceData pieceData = CustomPieceData.GetByPieceName(prefabName);
+                    if (pieceData == null)
+                        args.Context?.AddString($"Piece with name {prefabName} was not found");
+                    else
+                    {
+                        pieceData.SaveToDirectory(pluginDirectory.FullName);
+                        args.Context?.AddString($"Saved {prefabName} file to plugin directory");
+                    }
+                }
+            }, isCheat: false, isNetwork: false, onlyServer: false, isSecret: false, allowInDevBuild: false, () => CustomPieceData.GetBuildPieces().Select(piece => piece.name).ToList(), alwaysRefreshTabOptions: true, remoteCommand: false);
+        }
+
+        public static void SetupConfigWatcher()
+        {
+            string filter = $"*.json";
+
+            if (fileSystemWatcherPlugin == null)
+            {
+                fileSystemWatcherPlugin = new FileSystemWatcher(pluginDirectory.FullName, filter);
+                fileSystemWatcherPlugin.Changed += new FileSystemEventHandler(ReadConfigs);
+                fileSystemWatcherPlugin.Created += new FileSystemEventHandler(ReadConfigs);
+                fileSystemWatcherPlugin.Renamed += new RenamedEventHandler(ReadConfigs);
+                fileSystemWatcherPlugin.Deleted += new FileSystemEventHandler(ReadConfigs);
+                fileSystemWatcherPlugin.IncludeSubdirectories = true;
+                fileSystemWatcherPlugin.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            }
+
+            fileSystemWatcherPlugin.EnableRaisingEvents = modEnabled.Value;
+
+            if (fileSystemWatcherConfig == null)
+            {
+                if (configDirectory.Exists)
+                {
+                    fileSystemWatcherConfig = new FileSystemWatcher(configDirectory.FullName, filter);
+                    fileSystemWatcherConfig.Changed += new FileSystemEventHandler(ReadConfigs);
+                    fileSystemWatcherConfig.Created += new FileSystemEventHandler(ReadConfigs);
+                    fileSystemWatcherConfig.Renamed += new RenamedEventHandler(ReadConfigs);
+                    fileSystemWatcherConfig.Deleted += new FileSystemEventHandler(ReadConfigs);
+                    fileSystemWatcherConfig.IncludeSubdirectories = true;
+                    fileSystemWatcherConfig.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+                }
+            }
+
+            if (fileSystemWatcherConfig != null)
+                fileSystemWatcherConfig.EnableRaisingEvents = modEnabled.Value;
+
+            ReadConfigs(null, null);
+        }
+
+        private static void ReadConfigs(object sender, FileSystemEventArgs eargs)
+        {
+            Dictionary<string, string> localConfig = new Dictionary<string, string>();
+
+            FileInfo[] configFiles = pluginDirectory.GetFiles("*.json", SearchOption.AllDirectories);
+
+            if (configDirectory.Exists)
+                configFiles.AddRangeToArray(configDirectory.GetFiles("*.json", SearchOption.AllDirectories));
+
+            foreach (FileInfo file in configFiles)
+            {
+                LogInfo($"Found {file.FullName}");
+
+                try
+                {
+                    using (FileStream fs = new FileStream(file.FullName, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    using (StreamReader reader = new StreamReader(fs))
+                    {
+                        localConfig.Add(Path.GetFileNameWithoutExtension(file.Name), reader.ReadToEnd());
+                        reader.Close();
+                        fs.Dispose();
+                    }
+                }
+                catch (Exception e)
+                {
+                    LogWarning($"Error reading file ({file.FullName})! Error: {e.Message}");
+                }
+            }
+
+            configsJSON.AssignLocalValue(localConfig);
+        }
+
+        private static void LoadConfigs()
+        {
+            pieceData.Clear();
+
+            foreach (KeyValuePair<string, string> configJSON in configsJSON.Value)
+            {
+                try
+                {
+                    pieceData.Add(configJSON.Key, JsonUtility.FromJson<CustomPieceData>(configJSON.Value));
+                }
+                catch (Exception e)
+                {
+                    LogWarning($"Error parsing item ({configJSON.Key})! Error: {e.Message}");
+                }
+            }
+        }
+
+        private static void PatchPiece(Piece piece)
+        {
+            string name = Utils.GetPrefabName(piece.gameObject);
+            if (pieceData.ContainsKey(name))
+            {
+                LogInfo($"Patching {name}");
+                pieceData[name].PatchPiece(piece);
+            }
+
+            if (globalClipEverything.Value)
+                piece.m_clipEverything = true;
+
+            if (globalAllowedInDungeons.Value)
+                piece.m_allowedInDungeons = true;
+
+            if (globalRepairPiece.Value)
+                piece.m_repairPiece = true;
+
+            if (globalCanBeRemoved.Value)
+                piece.m_canBeRemoved = true;
+
+            if ((globalAshDamageImmune.Value || globalNoRoofWear.Value || globalNoSupportWear.Value) && piece.TryGetComponent(out WearNTear wnt))
+            {
+                if (globalAshDamageImmune.Value)
+                    wnt.m_ashDamageImmune = true;
+
+                if (globalNoRoofWear.Value)
+                    wnt.m_noRoofWear = false;
+
+                if (globalNoSupportWear.Value)
+                    wnt.m_noSupportWear = false;
+            }
+        }
+
+        private static void PatchPieces()
+        {
+            if (!(bool)ObjectDB.instance)
+                return;
+
+            SetupConfigWatcher();
+
+            FillCraftingStations();
+
+            foreach (GameObject go in CustomPieceData.GetBuildPieces())
+                if (go.TryGetComponent(out Piece piece))
+                    PatchPiece(piece);
+        }
+
+        [HarmonyPatch(typeof(ObjectDB), nameof(ObjectDB.CopyOtherDB))]
+        private static class CopyOtherDB_Patch
+        {
+            private static void Postfix()
+            {
+                if (!modEnabled.Value)
+                    return;
+
+                //PatchPieces();
+            }
+        }
+
+        [HarmonyPatch(typeof(ZNetScene), nameof(ZNetScene.Awake))]
+        [HarmonyPriority(Priority.Last)]
+        private static class ZNetScene_Awake_Patch
+        {
+            private static void Postfix()
+            {
+                if (!modEnabled.Value)
+                    return;
+
+                PatchPieces();
+            }
+        }
+
+        [HarmonyPatch(typeof(Piece), nameof(Piece.Awake))]
+        private static class Piece_Awake_PatchPiece
+        {
+            private static void Postfix(Piece __instance)
+            {
+                if (!modEnabled.Value)
+                    return;
+
+                PatchPiece(__instance);
+            }
+        }
+
+        private static void FillCraftingStations()
+        {
+            craftingStations.Clear();
+            foreach (Recipe recipe in ObjectDB.instance.m_recipes)
+            {
+                if (recipe?.m_craftingStation == null)
+                    continue;
+
+                if (craftingStations.ContainsKey(recipe.m_craftingStation.name))
+                    continue;
+
+                craftingStations[recipe.m_craftingStation.name] = recipe.m_craftingStation;
+                craftingStations[recipe.m_craftingStation.m_name] = recipe.m_craftingStation;
+                craftingStations[recipe.m_craftingStation.m_name.Substring(1)] = recipe.m_craftingStation;
+            }
+        }
+    }
+}
---
+++ CustomPieceData.cs
@@ -0,0 +1,306 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using static BuildPiecesCustomized.BuildPiecesCustomized;
+
+namespace BuildPiecesCustomized
+{
+    [Serializable]
+    internal class CustomPieceData
+    {
+        public string name;
+
+        public Piece.PieceCategory category;
+        public Piece.ComfortGroup comfortGroup;
+        public int comfort;
+        public bool groundPiece;
+        public bool allowAltGroundPlacement;
+        public bool groundOnly;
+        public bool cultivatedGroundOnly;
+        public bool waterPiece;
+        public bool clipGround;
+        public bool clipEverything;
+        public bool noInWater;
+        public bool notOnWood;
+        public bool notOnTiltingSurface;
+        public bool inCeilingOnly;
+        public bool notOnFloor;
+        public bool noClipping;
+        public bool onlyInTeleportArea;
+        public bool allowedInDungeons;
+        public float spaceRequirement;
+        public bool repairPiece;
+        public bool canBeRemoved = true;
+        public bool allowRotatedOverlap;
+        public bool vegetationGroundOnly;
+        public float blockRadius;
+        public int extraPlacementDistance;
+        public string station = "";
+        public Heightmap.Biome onlyInBiome;
+
+        public bool targetNonPlayerBuilt = true;
+        public bool primaryTarget;
+        public bool randomTarget = true;
+
+        public float health = 100f;
+        public bool noRoofWear = true;
+        public bool noSupportWear = true;
+        public WearNTear.MaterialType materialType;
+        public bool supports;
+        public Vector3 comOffset = Vector3.zero;
+        public float hitNoise;
+        public float destroyNoise;
+        public bool autoCreateFragments = true;
+        public List<string> damageModifiers = new List<string>();
+
+        public bool ashDamageImmune;
+        public bool ashDamageResist;
+        public bool burnable = true;
+        public int minToolTier;
+        public bool triggerPrivateArea = true;
+
+        public List<string> resources = new List<string>();
+
+        internal void PatchPiece(Piece piece)
+        {
+            piece.m_category = category;
+            piece.m_comfort = comfort;
+            piece.m_comfortGroup = comfortGroup;
+            piece.m_groundPiece = groundPiece;
+            piece.m_allowAltGroundPlacement = allowAltGroundPlacement;
+            piece.m_groundOnly = groundOnly;
+            piece.m_cultivatedGroundOnly = cultivatedGroundOnly;
+            piece.m_waterPiece = waterPiece;
+            piece.m_clipGround = clipGround;
+            piece.m_clipEverything = clipEverything;
+            piece.m_noInWater = noInWater;
+            piece.m_notOnWood = notOnWood;
+            piece.m_notOnTiltingSurface = notOnTiltingSurface;
+            piece.m_inCeilingOnly = inCeilingOnly;
+            piece.m_notOnFloor = notOnFloor;
+            piece.m_noClipping = noClipping;
+            piece.m_onlyInTeleportArea = onlyInTeleportArea;
+            piece.m_allowedInDungeons = allowedInDungeons;
+            piece.m_spaceRequirement = spaceRequirement;
+            piece.m_repairPiece = repairPiece;
+            piece.m_canBeRemoved = canBeRemoved;
+
+            piece.m_allowRotatedOverlap = allowRotatedOverlap;
+            piece.m_vegetationGroundOnly = vegetationGroundOnly;
+            piece.m_blockRadius = blockRadius;
+            piece.m_extraPlacementDistance = extraPlacementDistance;
+
+            piece.m_craftingStation = craftingStations.GetValueSafe(station);
+            piece.m_onlyInBiome = onlyInBiome;
+
+            piece.m_targetNonPlayerBuilt = targetNonPlayerBuilt;
+            piece.m_primaryTarget = primaryTarget;
+            piece.m_randomTarget = randomTarget;
+
+            List<Piece.Requirement> m_resources = new List<Piece.Requirement>();
+            foreach (string modString in resources)
+            {
+                string[] parts = modString.Split(':');
+
+                ItemDrop item = ObjectDB.instance?.GetItemPrefab(parts[0])?.GetComponent<ItemDrop>();
+                if (item == null)
+                    continue;
+
+                m_resources.Add(new Piece.Requirement()
+                {
+                    m_resItem = item,
+                    m_amount = parts.Length < 2 ? 1 : int.Parse(parts[1]),
+                    m_recover = parts.Length < 3 || bool.Parse(parts[2]),
+                });
+            }
+
+            piece.m_resources = m_resources.ToArray();
+
+            if (piece.TryGetComponent(out WearNTear wnt))
+            {
+                wnt.m_health = health;
+                wnt.m_noRoofWear = noRoofWear;
+                wnt.m_noSupportWear = noSupportWear;
+                wnt.m_materialType = materialType;
+                wnt.m_supports = supports;
+                wnt.m_comOffset = comOffset;
+                wnt.m_hitNoise = hitNoise;
+                wnt.m_destroyNoise = destroyNoise;
+                wnt.m_autoCreateFragments = autoCreateFragments;
+
+                wnt.m_ashDamageImmune = ashDamageImmune;
+                wnt.m_ashDamageResist = ashDamageResist;
+                wnt.m_burnable = burnable;
+                wnt.m_minToolTier = minToolTier;
+                wnt.m_triggerPrivateArea = triggerPrivateArea;
+
+                foreach (string modString in damageModifiers)
+                {
+                    string[] parts = modString.Split(':');
+                    if (parts.Length != 2)
+                        continue;
+
+                    HitData.DamageType type = (HitData.DamageType)Enum.Parse(typeof(HitData.DamageType), parts[0]);
+                    HitData.DamageModifier mod = (HitData.DamageModifier)Enum.Parse(typeof(HitData.DamageModifier), parts[1]);
+                    switch (type)
+                    {
+                        case HitData.DamageType.Blunt:
+                            wnt.m_damages.m_blunt = mod;
+                            break;
+                        case HitData.DamageType.Slash:
+                            wnt.m_damages.m_slash = mod;
+                            break;
+                        case HitData.DamageType.Pierce:
+                            wnt.m_damages.m_pierce = mod;
+                            break;
+                        case HitData.DamageType.Chop:
+                            wnt.m_damages.m_chop = mod;
+                            break;
+                        case HitData.DamageType.Pickaxe:
+                            wnt.m_damages.m_pickaxe = mod;
+                            break;
+                        case HitData.DamageType.Fire:
+                            wnt.m_damages.m_fire = mod;
+                            break;
+                        case HitData.DamageType.Frost:
+                            wnt.m_damages.m_frost = mod;
+                            break;
+                        case HitData.DamageType.Lightning:
+                            wnt.m_damages.m_lightning = mod;
+                            break;
+                        case HitData.DamageType.Poison:
+                            wnt.m_damages.m_poison = mod;
+                            break;
+                        case HitData.DamageType.Spirit:
+                            wnt.m_damages.m_spirit = mod;
+                            break;
+                    }
+                }
+
+            }
+        }
+
+        internal void SaveToDirectory(string directory)
+        {
+            string filename = Path.Combine(directory, $"{name}.json");
+
+            File.WriteAllText(filename, JsonUtility.ToJson(this, true));
+        }
+
+        internal CustomPieceData()
+        {
+
+        }
+
+        internal CustomPieceData(Piece piece)
+        {
+            name = piece.gameObject.name;
+            category = piece.m_category;
+            comfortGroup = piece.m_comfortGroup;
+            comfort = piece.m_comfort;
+            groundPiece = piece.m_groundPiece;
+            allowAltGroundPlacement = piece.m_allowAltGroundPlacement;
+            groundOnly = piece.m_groundOnly;
+            cultivatedGroundOnly = piece.m_cultivatedGroundOnly;
+            waterPiece = piece.m_waterPiece;
+            clipGround = piece.m_clipGround;
+            clipEverything = piece.m_clipEverything;
+            noInWater = piece.m_noInWater;
+            notOnWood = piece.m_notOnWood;
+            notOnTiltingSurface = piece.m_notOnTiltingSurface;
+            inCeilingOnly = piece.m_inCeilingOnly;
+            notOnFloor = piece.m_notOnFloor;
+            noClipping = piece.m_noClipping;
+            onlyInTeleportArea = piece.m_onlyInTeleportArea;
+            allowedInDungeons = piece.m_allowedInDungeons;
+            spaceRequirement = piece.m_spaceRequirement;
+            repairPiece = piece.m_repairPiece;
+            canBeRemoved = piece.m_canBeRemoved;
+            station = piece.m_craftingStation ? piece.m_craftingStation.m_name : "";
+            onlyInBiome = piece.m_onlyInBiome;
+
+            allowRotatedOverlap = piece.m_allowRotatedOverlap;
+            vegetationGroundOnly = piece.m_vegetationGroundOnly;
+            blockRadius = piece.m_blockRadius;
+            extraPlacementDistance = piece.m_extraPlacementDistance;
+
+            targetNonPlayerBuilt = piece.m_targetNonPlayerBuilt;
+            primaryTarget = piece.m_primaryTarget;
+            randomTarget = piece.m_randomTarget;
+
+            resources.AddRange(piece.m_resources.Select(req => { return $"{req.m_resItem.name}:{req.m_amount}:{req.m_recover}"; }));
+
+            if (piece.TryGetComponent(out WearNTear wnt))
+            {
+                health = wnt.m_health;
+                noRoofWear = wnt.m_noRoofWear;
+                noSupportWear = wnt.m_noSupportWear;
+                materialType = wnt.m_materialType;
+                supports = wnt.m_supports;
+                comOffset = wnt.m_comOffset;
+                hitNoise = wnt.m_hitNoise;
+                destroyNoise = wnt.m_destroyNoise;
+                autoCreateFragments = wnt.m_autoCreateFragments;
+
+                ashDamageImmune = wnt.m_ashDamageImmune;
+                ashDamageResist = wnt.m_ashDamageResist;
+                burnable = wnt.m_burnable;
+                minToolTier = wnt.m_minToolTier;
+                triggerPrivateArea = wnt.m_triggerPrivateArea;
+
+                damageModifiers = new List<string>()
+                {
+                    Enum.GetName(typeof(HitData.DamageType), HitData.DamageType.Blunt) + ":" + Enum.GetName(typeof(HitData.DamageModifier), wnt.m_damages.m_blunt),
+                    Enum.GetName(typeof(HitData.DamageType), HitData.DamageType.Slash) + ":" + Enum.GetName(typeof(HitData.DamageModifier), wnt.m_damages.m_slash),
+                    Enum.GetName(typeof(HitData.DamageType), HitData.DamageType.Pierce) + ":" + Enum.GetName(typeof(HitData.DamageModifier), wnt.m_damages.m_pierce),
+                    Enum.GetName(typeof(HitData.DamageType), HitData.DamageType.Chop) + ":" + Enum.GetName(typeof(HitData.DamageModifier), wnt.m_damages.m_chop),
+                    Enum.GetName(typeof(HitData.DamageType), HitData.DamageType.Pickaxe) + ":" + Enum.GetName(typeof(HitData.DamageModifier), wnt.m_damages.m_pickaxe),
+                    Enum.GetName(typeof(HitData.DamageType), HitData.DamageType.Fire) + ":" + Enum.GetName(typeof(HitData.DamageModifier), wnt.m_damages.m_fire),
+                    Enum.GetName(typeof(HitData.DamageType), HitData.DamageType.Frost) + ":" + Enum.GetName(typeof(HitData.DamageModifier), wnt.m_damages.m_frost),
+                    Enum.GetName(typeof(HitData.DamageType), HitData.DamageType.Lightning) + ":" + Enum.GetName(typeof(HitData.DamageModifier), wnt.m_damages.m_lightning),
+                    Enum.GetName(typeof(HitData.DamageType), HitData.DamageType.Poison) + ":" + Enum.GetName(typeof(HitData.DamageModifier), wnt.m_damages.m_poison),
+                    Enum.GetName(typeof(HitData.DamageType), HitData.DamageType.Spirit) + ":" + Enum.GetName(typeof(HitData.DamageModifier), wnt.m_damages.m_spirit)
+                };
+
+            }
+        }
+
+        internal static CustomPieceData GetByPieceName(string pieceName)
+        {
+            if (!(bool)ObjectDB.instance)
+                return null;
+
+            GameObject prefab = GetBuildPieces().First(buildPiece => buildPiece.name == pieceName);
+            if (prefab == null) 
+                return null;
+
+            if (!prefab.TryGetComponent(out Piece piece))
+                return null;
+
+            return new CustomPieceData(piece);
+        }
+
+        internal static List<GameObject> GetBuildPieces()
+        {
+            List<GameObject> pieces = new List<GameObject>();
+
+            HashSet<string> toolList = new HashSet<string>(toolsToPatchPieces.Value.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(c => { return c.Trim(); }));
+
+            foreach (string toolName in toolList)
+            {
+                ItemDrop tool = ObjectDB.instance.GetItemPrefab(toolName)?.GetComponent<ItemDrop>();
+
+                if (tool == null)
+                    continue;
+
+                pieces.AddRange(tool.m_itemData.m_shared.m_buildPieces.m_pieces);
+            }
+
+            return pieces;
+        }
+
+    }
+}
